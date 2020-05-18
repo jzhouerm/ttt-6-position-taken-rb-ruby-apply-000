784f40ce16883b1af27ad4c2908efada48e4c3c8
JULIA ZHOU <jzhouerm@hotmail.com>
2020-05-18 22:23:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,4 +2,6 @@
 def position_taken_taken(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     return false
-  else board[index] == 
+  else board[index] == "X" || board[index] == "O"
+    return true 
+  
